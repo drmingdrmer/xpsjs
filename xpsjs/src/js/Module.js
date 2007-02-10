68b6af4eb963aa318ad514e43b26ba064f3179b2
drdr.xp <drdr.xp@bd577780-a822-0410-873d-a18493aeb948>
2007-02-10 06:53:10 +0000

    Failed commit: Default
    git-svn-id: http://xpsjs.googlecode.com/svn/trunk@26 bd577780-a822-0410-873d-a18493aeb948

--- xpsjs/src/js/Module.js
+++ xpsjs/src/js/Module.js
@@ -16,124 +16,132 @@
  * @param {Array} modules Modules this module required.
  * @param {Object} hash function hash.usually an object.
  */
-window.Module = function (name, modules, hash){
+window.Module = function (name, modules, hash) {
 	if (typeof(modules) == "string") modules = modules.split(",");
 
 	this._name = name;
 	this._requiredModules = modules;
-	
+
 	//provider provides function hashes.
 	if (typeof(hash) == 'function') hash = hash(this, this._name);
-	
-	for (var i in hash){
+
+	for (var i in hash) {
 		this[i] = hash[i];
-		this[i].isOverridable 	= Module.createGetFunc(Module.isOverridable(this[i]));
-		this[i].getName 		= Module.createGetFunc(i);
-		this[i].getModule 		= Module.createGetFunc(this);
+		this[i].isOverridable = Module.createGetFunc(Module.isOverridable(this[i]));
+		this[i].getName = Module.createGetFunc(i);
+		this[i].getModule = Module.createGetFunc(this);
 		this[i].__$isModuleMethod = true;
 	}
-	
+
 	Module.assignModuleInstToName(name, this);
 	Module.queueInitJob(name, this);
-	Module.tryToInit();//try to mix
+	Module.tryToInit();
+	//try to mix
 };
 Module.loader = Loader.instance;
+//noinspection JSUnresolvedVariable
 Module.moduleRoot = $module;
 Module.moduleRoot.Module = Module;
-Module.getHostWin = function (){return Module.loader.getHostWin();};
-Module.getHostDoc = function (){return Module.loader.getHostDoc();};
+Module.getHostWin = function () {
+	return Module.loader.getHostWin();
+};
+Module.getHostDoc = function () {
+	return Module.loader.getHostDoc();
+};
 Module.initQueue = {};
 
-Module.assignModuleInstToName = function (name, module){
+Module.assignModuleInstToName = function (name, module) {
 	var r = Module.moduleRoot;
-	var ar = name.replace(/(^\.+)|(\.+$)/gi,"").replace(/\.+/gi,".").split(".");
-	for (var i=0; i<ar.length-1; i++) {
+	var ar = name.replace(/(^\.+)|(\.+$)/gi, "").replace(/\.+/gi, ".").split(".");
+	for (var i = 0; i < ar.length - 1; i++) {
 		var n = ar[i];
 		r = (r[n] = r[n] || {});
 	}
 	r[ar[i]] = module;
 };
 
-Module.isOverridable = function (func){
-	if (func == null) return true;
+Module.isOverridable = function (func) {
+	if (func === null) return true;
 	//var reg = ;
 	return /^function[^\(]*?\(([^\)]*?\,)*\s*\$overridable\)/.test(func.toString());
 };
 
-Module.createGetFunc = function (value){
-	return function (){return value};
+Module.createGetFunc = function (value) {
+	return function () {
+		return value
+	};
 };
 
-Module.get = function (name){
-	try{
-		return eval("Module.moduleRoot."+name);
-	} catch (e){
+Module.get = function (name) {
+	try {
+		return eval("Module.moduleRoot." + name);
+	} catch (e) {
 		return null;
 	}
 };
 
-Module.queueInitJob = function (name, module){
+Module.queueInitJob = function (name, module) {
 	var m = Module.initQueue[name] = module;
 	Module.loader.loadModules(m._requiredModules);
 };
 
-Module.require = function (modules){
+Module.require = function (modules) {
 	Module.loader.loadModules(modules || []);
 }
 
-Module.tryToInit = function (){
-	try{
+Module.tryToInit = function () {
+	try {
 		for (var i in Module.initQueue) {
-			if (Module.initQueue[i]!=null)
+			if (Module.initQueue[i] !== null)
 				Module.initModule(i);
 		}
-	} catch (e){
+	} catch (e) {
 	}
 };
 /**
-* mix needed module to this module. runs recersively.
-*/
-Module.initModule = function (name){
+ * mix needed module to this module. runs recersively.
+ */
+Module.initModule = function (name) {
 	var module = Module.get(name);
-	if (module==null) throw new Error(name + " hasnt loaded yet");
-	
+	if (module === null) throw new Error(name + " hasnt loaded yet");
+
 	//try to mix needed module to itself.
 	var md = module._requiredModules;
-	
-	for (var i=0; i<md.length; i++){
-		if (Module.initQueue[md[i]] != null)
+
+	for (var i = 0; i < md.length; i++) {
+		if (Module.initQueue[md[i]] !== null)
 			Module.initModule(md[i]);
 		Module.get(md[i]).mixTo(module);
-		
+
 	}
 	module._$initialize();
-	Module.initQueue[name] = null;//remove this module from init queue.
-	//log("init Ok : " + name);
+	Module.initQueue[name] = null;
+	//remove this module from init queue.
 };
 
 var o = {
-	_$initialize : function (){},
-		
-	mixTo : function (t){
+	_$initialize : function () {
+	},
+
+	mixTo : function (t) {
 		var isFunc = t instanceof Function;
 		if (!isFunc && (t.constructor != Module))
 			throw new Error("Module can only be mixed to Function or Module");
-		
+
 		if (isFunc)	t = t.prototype;
-		
+
 		for (var i in this) {
-			if (typeof(this[i]) != "function" || Module.prototype[i] != null) continue;	
-			if (t[i]==null) t[i]=this[i];
-			else if (t[i] != this[i] && !this[i].isOverridable())
+			if (typeof(this[i]) != "function" || Module.prototype[i] !== null) continue;
+			if (t[i] === null) t[i] = this[i]; else if (t[i] != this[i] && !this[i].isOverridable())
 				throw new Error("memeber already exists : " + i + " in : " + (t._name || t.constructor) + " from : " + this._name);
 		}
 	},
-	
-	compatableTo : function (obj){
-		if (obj == null) return false;
+
+	compatableTo : function (obj) {
+		if (obj === null) return false;
 		if (typeof(obj) == "function") obj = obj.prototype;
-		for (var i in this){
-			if (this[i].__$isModuleMethod && Module.prototype[i] == null){
+		for (var i in this) {
+			if (this[i].__$isModuleMethod && Module.prototype[i] === null) {
 				if (typeof(obj[i]) != "function") {
 					//alert(i + ":" + obj[i]);
 					return false;
@@ -143,16 +151,17 @@ var o = {
 		return true;
 	},
 
-	newInst : function (constr){
+	newInst : function (constr) {
 		var clazz = this.clz(constr);
 		return new clazz();
 	},
 
-	clz : function (obj){
-		obj = obj || (function (){});
+	clz : function (obj) {
+		obj = obj || (function () {
+		});
 		var constr;
-		if (typeof(obj) == "object"){
-			constr = function (){
+		if (typeof(obj) == "object") {
+			constr = function () {
 				for (var i in obj) this[i] = obj[i];
 			}
 		} else constr = obj;
