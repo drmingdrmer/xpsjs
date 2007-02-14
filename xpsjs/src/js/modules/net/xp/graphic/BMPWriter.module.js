c835679af4f2aa22eba319650ea54223a2d9622a
drdr.xp <drdr.xp@bd577780-a822-0410-873d-a18493aeb948>
2007-02-14 04:17:41 +0000

    Rejected commit: Default
    git-svn-id: http://xpsjs.googlecode.com/svn/trunk@49 bd577780-a822-0410-873d-a18493aeb948

---
+++ xpsjs/src/js/modules/net/xp/graphic/BMPWriter.module.js
@@ -0,0 +1,19 @@
+new Module("net.xp.graphic.BMPWriter",
+[
+    "net.xp.core.Core"
+],function ($this,$name){return {
+	_$initialize : function (){
+	},
+
+	setSize : function (o){
+
+	},
+
+	setPixel : function (position, color){
+
+	},
+
+	toArray : function (){
+
+	}
+}});
---
+++ xpsjs/src/js/modules/net/xp/graphic/GifWriter.module.js
@@ -0,0 +1,26 @@
+new Module("net.xp.graphic.GifWriter",
+[
+    "net.xp.core.Core"
+],function ($this,$name){return {
+	_$initialize : function (){
+	},
+
+	_$defaultConstructor : function (){
+	},
+
+	setSize : function (o){
+
+	},
+
+	setPixel : function (position, color){
+
+	},
+
+	toArray : function (){
+		
+	}
+
+	
+
+
+}});
