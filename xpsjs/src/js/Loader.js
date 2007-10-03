81bae6f7de7ca7db109de74eeacd067a53f75b3e
drdr.xp <drdr.xp@bd577780-a822-0410-873d-a18493aeb948>
2007-10-03 06:34:39 +0000

    simplify Loader.js
    git-svn-id: http://xpsjs.googlecode.com/svn/trunk@103 bd577780-a822-0410-873d-a18493aeb948

--- xpsjs/src/js/Loader.js
+++ xpsjs/src/js/Loader.js
@@ -10,6 +10,7 @@
  * TODO simplify loading process. Load resource for only Module is ok.
  * TODO ensure loading sequence is correct. thinking about XHR method
  * TODO multi - base
+ * TODO use path instead of config.path.
  *
  */
 window.ModuleLoader = function(config) {
@@ -18,27 +19,24 @@ window.ModuleLoader = function(config) {
   this.hostWin = window;
   this.toLoad = [];
   this.loaded = {};
-  this.inLoading = false;
-  this.nonInited = 0;
+  // this.inLoading = false;
   this.ready = false;
   this.id = 0;
 
-  this.config = config || {};
+  var cfg = (this.config = (config || {}));
+  cfg.$module = true;
 
-  var tempHolders = {$module : true};
-  (this.config.holders || "").replace(/[^\,]*/g, function(a){tempHolders[a] = true;});
-  this.config.holders = tempHolders;
 
-  var path = (this.config.path = this.config.path || {});
+  var path = (this.path = cfg.path || {});
   path.base = null;
   path.js = path.js || "js";
   path.modules = path.modules || "modules";
   path.blankPage = path.blankPage || "blank.html";
   this.initBaseUrl();
-  this.initHolders(this.config);
+  this.createHolder("$module");
 }
 
-var p = {
+window.ModuleLoader.prototype = {
   $               : function (id, doc) { return (doc || this.getHostDoc()).getElementById(id); },
 
   $n              : function (name, doc) { return (doc || this.getHostDoc()).getElementsByTagName(name); },
@@ -47,54 +45,65 @@ var p = {
 
   getHostDoc      : function () { return this.getHostWin().document; },
 
-  getBase         : function () { return this.config.path.base; },
+  getBase         : function () { return this.path.base; },
 
-  getBlankPageUrl : function () { return this.simplifyURL(this.config.path.blankPage); },
+  getBlankPageUrl : function () { return this.simplifyURL(this.path.blankPage); },
+
+  bind            : function (func, thiz, args){ return function(){func.apply(thiz, args)}; }, 
+
+  // addIframeOnload : function (ifm, func){
+    // if (ifm.addEventListener) {
+      // ifm.addEventListener("load", func, true);
+    // } else {
+      // ifm.attachEvent("onload", func);
+    // }
+  // }, 
 
   createHolder : function (name) {
     var doc = this.getHostDoc();
-    if (this.$(name) != null) {
-      return this.config.holders[name] = this.$(name).contentWindow;
-    }
+    if (this.$(name) != null) return;
 
     var e = doc.createElement("div");
-    e.innerHTML = "<iframe id='" + name + "' name='" + name + "' " + "src='" + this.config.path.base + "/blank.html'" + " style='display : block;'" + "><\/iframe>";
+    e.innerHTML = "<iframe \
+        id='" + name + "' \
+        name='" + name + "' \
+        style='display : block;'><\/iframe>";
+        // src='" + this.getBlankPageUrl() + "' \
     var ifm = e.firstChild;
     doc.body.appendChild(ifm);
-    this.config.holders[name] = ifm.contentWindow;
+    this.$module = ifm.contentWindow;
 
-    var loader = this;
-    var onloadFunc = function () {
-      ifm.contentWindow.ModuleLoader = loader.getHostWin().ModuleLoader;
-      if (--loader.nonInited == 0) loader.ready=true;
-    }
-    if (ifm.addEventListener) {
-      ifm.addEventListener("load", onloadFunc, true);
-    } else {
-      ifm.attachEvent("onload", onloadFunc);
-    }
-    this.nonInited++;
-    return ifm;
+    var idoc = this.$module.document;
+    idoc.open();
+    idoc.close();
+
+
+    this.ready = true;
+
+    // var loader = this;
+    // var onloadFunc = function () {
+      // // TODO start load js
+      // ifm.contentWindow.ModuleLoader = loader.getHostWin().ModuleLoader;
+      // if (--loader.nonInited == 0) loader.ready=true;
+    // }
+    // this.addIframeOnload(ifm, onloadFunc)
   },
 
-  initHolders: function (h) {
-    for (var i in this.config.holders)
-      if (this.config.holders[i] === true) this.createHolder(i);
-  },
-
+  /**
+   * TODO refresh urls
+   */
   initBaseUrl : function () {
     var isLocal = location.protocol.indexOf("file") != -1;
 
-    //href url base
-    var cur = location.href.replace(/\/[^\/]*$/, "");
+    var cur = location.href.replace(/\/[^\/]*$/, ""); //href url base
 
-    var s = this.$n("script"),posi;
+    var s = this.$n("script"), posi;
     for (var i = 0; i < s.length; i++)
       if ((posi = s[i].src.lastIndexOf("Loader.js")) >= 0) break;
 
     var loaderPath = s[i].src.substr(0, posi - 1);
 
-    if (/^\//.test(loaderPath)) {//absolute path
+    if (/^\//.test(loaderPath)) {//absolute path from site root
       cur = location.protocol + "://" + (isLocal ? "" : location.host);
 
       //create absolute path
@@ -103,20 +112,27 @@ var p = {
       cur = loaderPath;
     } else cur += "/" + loaderPath; //relative path
 
-    this.config.path.base = this.simplifyURL(cur).replace(/\/[^\/]*$/, "");
+    //upper folder
+    this.path.base = this.simplifyURL(cur).replace(/\/[^\/]*$/, "");
+    var path = this.path;
+    path.blankPage = this.getBase() + "/" + path.blankPage;
+    path.js = this.getBase() + "/" + path.js;
+    path.modules = path.js + "/" + path.modules;
+
   },
 
   simplifyURL : function (url) {
-    if (url.indexOf("://") == -1) url = this.config.path.base + "/" + url;
-    while (/\.\.\//.test(url)) url = url.replace(/\/[^\/:]*\/\.\.\//gi, "/");
+    if (url.indexOf("://") == -1) url = this.getBase() + "/" + url;
+    while (/\.\.\//.test(url)) 
+      url = url.replace(/\/[^\/:]*\/\.\.\//gi, "/");
     return url;
   },
 
   getWinByName : function (winName) {
-    var win = this.config.holders[winName];
-    if (win == null) win = this.hostWin[winName];
-    if (win == null) win = this.getHostWin();
-    return win;
+    var win;
+    if (winName == "$module") return this.$module;
+    // var win = this.config.holders[winName];
+    return win || this.hostWin[winName] || this.getHostWin();
   },
 
   createScript : function (doc, hash) {
@@ -125,21 +141,23 @@ var p = {
 
     var scriptElement = doc.createElement("script");
     scriptElement.id = ++this.id;
-    if (hash.url != null) scriptElement.src = hash.url;
-    else if (hash.script != null) scriptElement.innerHTML = hash.script;
+    if (hash.url != null) 
+      scriptElement.src = hash.url;
+    else if (hash.script != null) 
+      scriptElement.innerHTML = hash.script;
 
     this.$n("head", doc)[0].appendChild(scriptElement);
     return scriptElement;
   },
 
   loadNextJS : function () {
-    if (!this.ready){
-      this.getHostWin().setTimeout(function (){
-        ModuleLoader.instance.loadNextJS();
-      },50);
-      return;
-    }
-    this.inLoading = true;
+    // if (!this.ready){
+      // this.getHostWin().setTimeout(function (){
+        // ModuleLoader.instance.loadNextJS();
+      // },50);
+      // return;
+    // }
+    // this.inLoading = true;
     
     var list = this.toLoad;
     while (list.length > 0) {
@@ -147,22 +165,40 @@ var p = {
 
       var win = this.getWinByName(winName);
 
-      this.createScript(win.document, {url:url});
-      this.addTriggerLoadFinishScript(winName, url);
+      //script element
+      var se = this.createScript(win.document, {url:url});
+      se.onload = this.bind(this._onFinishLoadOne, this, [se]);
+      // se.onload = function (){alert(1);}
+      // this.addTriggerLoadFinishScript(winName, url);
     }
   },
 
   loadJS : function (url, winName) {
-    url = this.simplifyURL(this.config.path.js + "/" + url);
+    if (url.indexOf("://") == -1) 
+      url = this.path.js + "/" +  url;
+    url = this.simplifyURL(url);
+
     var symbol = url + "_window_name_" + winName;
     if (this.loaded[symbol]) return false;
 
+
+    var win = this.getWinByName(winName);
+
+    //script element
+    var se = this.createScript(win.document, {url:url});
+    se.onload = this.bind(this._onFinishLoadOne, this, [se, url]);
+
+    this.loaded[symbol] = true;
+
+    return;
+
     this.toLoad.push({
-      url:url,
-      winName:winName,
-      toString:function () {
-        return this.winName + " : " + this.url + "";
-      }});
+        url:url,
+        winName:winName,
+        toString:function () {
+          return this.winName + " : " + this.url + "";
+        }
+      });
     this.loaded[symbol] = true;
 
     //start load if no loading job in progressing.
@@ -171,7 +207,7 @@ var p = {
 
   loadModule : function (mName) {
     mName = mName.replace(/\./gi, "/").replace(/\*$/,"_All") + ".module.js";
-    return this.loadJS(this.config.path.modules + "/" + mName, "$module");
+    return this.loadJS(this.path.modules + "/" + mName, "$module");
   },
 
   loadModules : function (ms) {
@@ -181,35 +217,36 @@ var p = {
     }
   },
 
-
   addOnload : function (func){
     var ar = this.config.onloadFuncs = (this.config.onloadFuncs || []);
     ar.push(func);
   },
 
-  /**
-   * add a trigger to notify loader that a script load finished by invoking Loader.onLoadFinishOne
-   * @param {Object} winName
-   * @param {Object} msg
-   */
-  addTriggerLoadFinishScript : function (winName, msg) {
-    var win = this.getWinByName(winName);
-    var scr = this.createScript(win.document, {
-      script : "window.Loader.instance._onFinishLoadOne(" + (this.id+1)  + ",'" +  msg + "');"
-    });
-  },
+  // /**
+   // * add a trigger to notify loader that a script load finished by invoking Loader.onLoadFinishOne
+   // * @param {Object} winName
+   // * @param {Object} msg
+   // */
+  // addTriggerLoadFinishScript : function (winName, msg) {
+    // var win = this.getWinByName(winName);
+    // var scr = this.createScript(win.document, {
+      // script : "window.Loader.instance._onFinishLoadOne(" + (this.id+1)  + ",'" +  msg + "');"
+    // });
+  // },
 
   /**
    * event handler
    * invoked when script load finished.
    * @param {Object} msg
    */
-  _onFinishLoadOne : function (id, msg) {
+  _onFinishLoadOne : function (se, url) {
+    alert(se + url);
+    return;
     if (id < this.id) {
       this.loadNextJS();
       return;
     }
-    this.inLoading = false;
+    // this.inLoading = false;
     //noinspection JSUnresolvedVariable,JSUnresolvedFunction
     this.config.onLoadFinish && this.config.onLoadFinish();
     if (this.config.onloadFuncs){
@@ -220,5 +257,10 @@ var p = {
   }
 }
 
-var pr = ModuleLoader.prototype;
-for (var i in p) if (pr[i] == null) pr[i] = p[i];
+// (function (){
+    // var ins = new ModuleLoader();
+    // ins.loadJS("ModuleConfig.js");
+    // ins.loadJS("Module.js");
+// })()
+
+
