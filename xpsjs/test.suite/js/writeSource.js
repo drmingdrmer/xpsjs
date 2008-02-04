d305f024a2914adb7287e2a8a7a75bf3d1413ac0
drdr.xp <drdr.xp@bd577780-a822-0410-873d-a18493aeb948>
2008-02-04 09:19:42 +0000

    git-svn-id: http://xpsjs.googlecode.com/svn/trunk@114 bd577780-a822-0410-873d-a18493aeb948

---
+++ xpsjs/test.suite/js/writeSource.js
@@ -0,0 +1,4 @@
+document.write('\
+<link rel="stylesheet" type="text/css" href="../../jsunit/css/jsUnitStyle.css">\
+<script type="application/javascript;version=1.7"  src="../../jsunit/app/jsUnitCore.js"></script>');
+
