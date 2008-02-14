10e9653efcd87c4a5135f6339721e3948575230b
drdr.xp <drdr.xp@bd577780-a822-0410-873d-a18493aeb948>
2008-02-14 03:29:34 +0000

    git-svn-id: http://xpsjs.googlecode.com/svn/trunk@136 bd577780-a822-0410-873d-a18493aeb948

--- xpsjs/src/js/modules/net/xp/core/EmbedFunc.module.js
+++ xpsjs/src/js/modules/net/xp/core/EmbedFunc.module.js
@@ -1,8 +1,62 @@
 new Module("net.xp.core.EmbedFunc", [
 ],function ($t, $n, $p, $g, $r, $c){
   /* private */
+
+  function getFuncBody(fstr, withBrace) {
+
+    var o = withBrace ? 0 : 1;
+
+    var i = fstr.indexOf("{");
+    var j = fstr.indexOf("}");
+
+    return fstr.substring(i+o, j+1-o);
+  }
+
+  function getFuncParam(fstr){
+
+    var i = fstr.indexOf("(");
+    var j = fstr.indexOf(")");
+
+    return fstr.substring(i+1, j).split(/[, ]+/);
+  }
+
+
+
+
   return {
     /* public */
+
+
+    embedFunc : function (ftmp, femb){
+      if (!ftmp || !femb) 
+	throw new Error("need 2 arguments but : " + ftmp + " and " + femb);
+
+      if (ftmp.constructor != Function || femb.constructor != Function) 
+	throw new Error("2 arguments must all be function but : " + ftmp + " and " + femb); 
+
+      if (femb.name == "") 
+	throw new Error ("the embeded function must hv name");
+
+      var fts = ftmp.toString();
+      var fes = femb.toString();
+
+      var ftparam = getFuncParam(fts);
+
+      var ftbody = getFuncBody(fts);
+      var febody = getFuncBody(fes);
+
+      /* TODO for now no parameter support */
+      var invStr = "\\b(" + femb.name + "\\b\\s*\\(\\))";
+      var invLineStr = "(.*)" + invStr + "(.*)";
+
+      var invLineReg = new RegExp(invLineStr, "g");
+
+      var newFB = ftbody.replace(invLineReg, "try{" + febody + "}catch(_ret_val){__return_value__ = _ret_val;} $1 __return_value__ $3");
+
+      var newFunc = Function.apply(ftparam.concat([newFB]));
+      console.log(newFunc.toString());
+
+    }
     
   }});
 
--- xpsjs/test.suite/modules/m.net.xp.core.EmbedFunc.js
+++ xpsjs/test.suite/modules/m.net.xp.core.EmbedFunc.js
@@ -1,10 +1,22 @@
 function func_template(a, b, c){
 
-}
-function func_embed(a, b, c){
+  var t = func_embed(1, 2);
 
+  return t;
 }
+
+function func_embed(a, b, c){
+  return a + b;
+}
+
 function test_embed_func(){
   var m = Module.get("net.xp.core.EmbedFunc");
 
+  var nf = m.embedFunc(func_template, func_embed);
+
+  var v = nf(1, 2, 3);
+
+  assertEquals("evaluate created function", 
+    3, 
+    v);
 }
